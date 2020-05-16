5566dc8251ca27e395ee6125f5599f4ccc302b55
Jorge Cruz <jlcruz7125@gmail.com>
2020-05-15 22:21:15 -0400

    Movie Service

--- src/app/services/movies.service.spec.ts
+++ src/app/services/movies.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthzeroService } from './authzero.service';
+import { MoviesService } from './movies.service';
 
-describe('AuthzeroService', () => {
-  let service: AuthzeroService;
+describe('MoviesService', () => {
+  let service: MoviesService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthzeroService);
+    service = TestBed.inject(MoviesService);
   });
 
   it('should be created', () => {
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -3,20 +3,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthService } from './auth.service';
 import { environment } from 'src/environments/environment';
 
-
-export interface Actor{
+export interface Movie{
   id:number,
-  name:string,
-  age:number,
-  gender:string,
+  title:string,
+  released:Date,
   picture_link:string,
-  bio:string
+  synopsis:string
 }
 
 @Injectable({
   providedIn: 'root'
 })
-export class ActorsService {
+export class MoviesService {
 
   url = environment.apiServerUrl;
 
@@ -32,40 +30,41 @@ export class ActorsService {
     return header;
   }
 
-  getActors(){
+
+  getMovies(){
     console.log(this.auth.activeJWT());
-    if (this.auth.can('read:actors')) {
-      this.http.get(this.url + '/actors', this.getHeaders())
+    if (this.auth.can('read:movies')) {
+      this.http.get(this.url + '/movies', this.getHeaders())
       .subscribe((res: any) => {
         console.log("RES", res);
       });
     }
   }
 
-  deleteActor(actor:Actor){
+  deleteActor(movie:Movie){
     console.log(this.auth.activeJWT());
-    if (this.auth.can('delete:actors')) {
-      this.http.delete(this.url + '/actors/' + actor.id, this.getHeaders())
+    if (this.auth.can('delete:movies')) {
+      this.http.delete(this.url + '/movies/' + movie.id, this.getHeaders())
       .subscribe((res: any) => {
         console.log("RES", res);
       });
     }
   }
 
-  saveActor(actor:Actor){
+  saveActor(movie:Movie){
     console.log(this.auth.activeJWT());
-    if(actor.id > 0){
+    if(movie.id > 0){
       // Patch
-      if (this.auth.can('patch:actors')) {
-        this.http.patch(this.url + '/actors/' + actor.id, actor, this.getHeaders())
+      if (this.auth.can('patch:movies')) {
+        this.http.patch(this.url + '/actors/' + movie.id, movie, this.getHeaders())
         .subscribe((res: any) => {
           console.log("RES", res);
         });
       }
     }else{
       // Post
-      if (this.auth.can('create:actors')) {
-        this.http.post(this.url + '/actors/', actor, this.getHeaders())
+      if (this.auth.can('create:movies')) {
+        this.http.post(this.url + '/actors/', movie, this.getHeaders())
         .subscribe((res: any) => {
           console.log("RES", res);
         });
